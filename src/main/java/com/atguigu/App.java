74854a86e67b3027bdea287de74136472c4241ba
Moyzzz <2416652912@qq.com>
2024-09-04 13:28:39 +0800

    master version

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Git!" );
         System.out.println("up to date!");
     }
 }
